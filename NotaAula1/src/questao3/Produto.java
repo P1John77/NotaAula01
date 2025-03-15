7795f04ae3d8342845efd48cffcd8faf4a1b160c
John <johncarlosd42@gmail.com>
2025-03-14 23:18:13 -0300

    Update Produto.java

--- NotaAula1/src/questao3/Produto.java
+++ NotaAula1/src/questao3/Produto.java
@@ -1,4 +1,37 @@
-package questao3;
+import java.util.Scanner;
 
 public class Produto {
+    // Atributos sem private
+    String nome;
+    double preco;
+    int quantidadeEmEstoque;
+
+
+    public Produto(String nome, double preco, int quantidadeEmEstoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public void adicionarEstoque(int quantidade) {
+        if (quantidade > 0) {
+            quantidadeEmEstoque += quantidade;
+            System.out.println(quantidade + " unidades adicionadas ao estoque.");
+        } else {
+            System.out.println("A quantidade deve ser maior que zero.");
+        }
+    }
+
+    public void vender(int quantidade) {
+        if (quantidade > 0 && quantidade <= quantidadeEmEstoque) {
+            quantidadeEmEstoque -= quantidade;
+            System.out.println(quantidade + " unidades vendidas.");
+        } else {
+            System.out.println("Quantidade inválida ou estoque insuficiente.");
+        }
+    }
+
+    public void exibirEstoque() {
+        System.out.println("Estoque atual de " + nome + ": " + quantidadeEmEstoque + " unidades.");
+    }
 }
