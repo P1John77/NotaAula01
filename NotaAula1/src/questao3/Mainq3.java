373853a89799365b679136c096422d72f51a2f91
John <johncarlosd42@gmail.com>
2025-03-14 23:19:22 -0300

    Update Mainq3.java

--- NotaAula1/src/questao3/Mainq3.java
+++ NotaAula1/src/questao3/Mainq3.java
@@ -1,4 +1,47 @@
-package questao3;
+import java.util.Scanner;
 
-public class Mainq3 {
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Produto produto = new Produto("Notebook", 2500.00, 10);
+
+        produto.exibirEstoque();
+
+        while (true) {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1 - Adicionar estoque");
+            System.out.println("2 - Vender produtos");
+            System.out.println("3 - Exibir estoque");
+            System.out.println("4 - Sair");
+            System.out.print("Opção: ");
+            int opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Quantidade a adicionar: ");
+                    int quantidadeAdicionar = sc.nextInt();
+                    produto.adicionarEstoque(quantidadeAdicionar);
+                    break;
+
+                case 2:
+                    System.out.print("Quantidade a vender: ");
+                    int quantidadeVender = sc.nextInt();
+                    produto.vender(quantidadeVender);
+                    break;
+
+                case 3:
+                    produto.exibirEstoque();
+                    break;
+
+                case 4:
+                    System.out.println("Saindo...");
+                    sc.close();
+                    return;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
 }
